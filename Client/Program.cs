698f4d98ab2648b55aba23b86ed294c46e40989d
kashiiitech <kashiiitech@gmail.com>
2025-01-16 16:01:49 +0500

    Creating a gRPC channel

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+
 Console.WriteLine("Hello, World!");
+
+// creating gRPC channel
+using var channel = GrpcChannel.ForAddress("https://localhost:7226");
